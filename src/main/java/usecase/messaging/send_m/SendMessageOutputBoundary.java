d4299ac9983b0f8b27922ff0d8a84ee020cc7db1
shidafuyang-alt <shidafuyang@gmail.com>
2025-12-02 00:48:35 -0500

    send javadocs added

--- src/main/java/usecase/messaging/send_m/SendMessageOutputBoundary.java
+++
@@ -1,6 +0,0 @@
-package usecase.messaging.send_m;
-
-public interface SendMessageOutputBoundary {
-    void prepareSuccessView(SendMessageOutputData outputData);
-    void prepareFailView(String errorMessage);
-}
